6b929b80ba7b64c7ab7ffe5bf8c2b61e48a1b3b8
Renos Efstathiou <it164826@it.teithe.gr>
2021-11-21 04:41:37 +0200

    Created the header, footer and products section of the home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import { Container } from 'react-bootstrap';
+
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import Footer from './components/Footer';
+
 const App = () => {
   return (
     <>
-      <h1> Welcome to ReCommerce</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
