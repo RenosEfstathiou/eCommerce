fb9702869cac5b2fa5f6e73b03bfb21e8e87c722
Renos Efstathiou <it164826@it.teithe.gr>
2021-11-24 23:31:40 +0200

    Implemented the functionality required to connect to mongoDB and created user, product and order Schemas

---
+++ backend/models/userModel.js
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default User = model('User', userSchema);
