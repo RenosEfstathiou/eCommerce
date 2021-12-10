127e67e3389a8a3b9d6ba7b17fd0dea85d336e78
Renos Efstathiou <it164826@it.teithe.gr>
2021-12-10 15:21:45 +0200

    Created the store functionality for user login and log out

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,16 +5,20 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 // reducers
 import { productListReducer, productDetailsReducer } from './reducers/productReducers.js';
 import { cartReducer } from './reducers/cartReducers.js';
+import { userLoginReducer } from './reducers/userReducers.js';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  userLogin: userLoginReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
 
-const initialState = { cart: { cartItems: cartItemsFromStorage } };
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
+
+const initialState = { cart: { cartItems: cartItemsFromStorage }, userLogin: { userInfo: userInfoFromStorage } };
 
 const middleware = [thunk];
 
